b67dd1967ad2e82ba148b548b587aae09706ed6d
gauravchaturvedi <gkchat10@gmail.com>
2017-10-06 03:46:38 -0400

    Assignment 4

--- src/main/java/api/ReceiptSuggestionResponse.java
+++ src/main/java/api/ReceiptSuggestionResponse.java
@@ -13,9 +13,25 @@ public class ReceiptSuggestionResponse {
     @JsonProperty
     public final BigDecimal amount;
 
+    @JsonProperty
+    public final Integer v1;
 
-    public ReceiptSuggestionResponse(String merchantName, BigDecimal amount) {
+    @JsonProperty
+    public final Integer v2;
+
+    @JsonProperty
+    public final Integer v3;
+
+    @JsonProperty
+    public final Integer v4;
+
+
+    public ReceiptSuggestionResponse(String merchantName, BigDecimal amount, Integer v1, Integer v2, Integer v3, Integer v4) {
         this.merchantName = merchantName;
         this.amount = amount;
+        this.v1 = v1;
+        this.v2= v2;
+        this.v3 = v3;
+        this.v4 = v4;
     }
 }
--- src/main/java/controllers/ReceiptImageController.java
+++ src/main/java/controllers/ReceiptImageController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import api.ReceiptSuggestionResponse;
+import java.util.List;
 import com.google.cloud.vision.v1.*;
 import com.google.protobuf.ByteString;
 import java.math.BigDecimal;
@@ -38,26 +39,50 @@ public class ReceiptImageController {
      */
     @POST
     public ReceiptSuggestionResponse parseReceipt(@NotEmpty String base64EncodedImage) throws Exception {
-        Image img = Image.newBuilder().setContent(ByteString.copyFrom(Base64.getDecoder().decode(base64EncodedImage))).build();
+        Image img = Image.newBuilder().setContent(ByteString.copyFrom(Base64.getDecoder().decode(base64EncodedImage.replaceFirst("data:image/png;base64,", "")))).build();
         AnnotateImageRequest request = this.requestBuilder.setImage(img).build();
 
         try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
             BatchAnnotateImagesResponse responses = client.batchAnnotateImages(Collections.singletonList(request));
             AnnotateImageResponse res = responses.getResponses(0);
 
-            String merchantName = null;
+            String merchantName = "";
             BigDecimal amount = null;
 
             // Your Algo Here!!
             // Sort text annotations by bounding polygon.  Top-most non-decimal text is the merchant
             // bottom-most decimal text is the total amount
-            for (EntityAnnotation annotation : res.getTextAnnotationsList()) {
-                out.printf("Position : %s\n", annotation.getBoundingPoly());
-                out.printf("Text: %s\n", annotation.getDescription());
+
+            List<EntityAnnotation> textAnnotations = res.getTextAnnotationsList();
+
+            for (int i = 0; i < textAnnotations.size(); i++) {
+                EntityAnnotation textAnnotation = textAnnotations.get(i);
+                if(merchantName.isEmpty()) {
+                    if (!textAnnotation.getDescription().split("[\n\r\t]")[0].contains(".")) {
+                        merchantName = textAnnotation.getDescription().split("[\n\r\t]")[0];
+                    }
+                }
+
+                if (textAnnotation.getDescription().matches("[0-9]*\\.[0-9]{2}")) {
+                    amount = new BigDecimal(textAnnotation.getDescription());
+                }
             }
 
-            //TextAnnotation fullTextAnnotation = res.getFullTextAnnotation();
-            return new ReceiptSuggestionResponse(merchantName, amount);
+            String frame = res.getTextAnnotationsList().get(0).getBoundingPoly().toString();
+            String[] vertices = frame.split("vertices ");
+            String firstvertice = vertices[1];
+            String secondvertice = vertices[3];
+            String[] firstVerticeStringValue = firstvertice.split("\n");
+            String[] secondVerticeStringValue = secondvertice.split("\n");
+
+            // Extract vertex integer values
+            Integer v1 = Integer.parseInt(firstVerticeStringValue[1].split(": ")[1]);
+            Integer v2 = Integer.parseInt(firstVerticeStringValue[2].split(": ")[1]);
+            Integer v3 = Integer.parseInt(secondVerticeStringValue[1].split(": ")[1]);
+            Integer v4 = Integer.parseInt(secondVerticeStringValue[2].split(": ")[1]);
+
+            // return new ReceiptSuggestionResponse(merchantName, amount, 1, 2, 3, 4);
+            return new ReceiptSuggestionResponse(merchantName, amount, v1, v2, v3, v4);
         }
     }
 }
